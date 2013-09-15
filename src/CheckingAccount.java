f75d4625dac97e61372d9608b38fd1c78adda10d
drakoz <mat.mello93@gmail.com>
2013-09-15 09:25:31 -0300

    Interface, main e coracao do programa

---
+++ src/CheckingAccount.java
@@ -0,0 +1,35 @@
+public class CheckingAccount extends Object{
+	private String accountNumber;
+	private String accountHolder;
+	private double currentBalance;
+	
+	public CheckingAccount(String aNumber,String aHolder,double cBalance){
+		accountNumber = aNumber;
+		accountHolder = aHolder;
+		currentBalance = cBalance;
+	}
+	public double getBalance(){
+		return this.currentBalance;
+	}
+	public String getAccntNumber(){
+		return this.accountNumber;
+		
+	}
+	public void deposit(double dep){
+		this.currentBalance=currentBalance + dep;
+	}
+	public  double processCheque(int chequeAmount){
+		if(this.currentBalance<1000)
+		{
+			this.currentBalance = currentBalance -(chequeAmount+0.15);
+			return currentBalance;
+		}
+		else
+			this.currentBalance = currentBalance -chequeAmount;
+			return currentBalance;
+	}
+	@Override
+	public String toString(){
+		return String.format("%s: %s \n%s: %s \n%s: %s","Name",accountHolder,"Account Number",accountNumber,"Account Balance",currentBalance);
+	}
+}
